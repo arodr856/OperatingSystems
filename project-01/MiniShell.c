4cb0742f3982b2047816ff5ac9ef956736e5656a
arodr856 <arodr856@gmail.com>
2018-10-03 08:19:57 -0700

    simple minishell impl.

--- project-01/MiniShell.c
+++ project-01/MiniShell.c
@@ -7,13 +7,21 @@
 int main(int argc, char** argv){
 
     char command[5];
-    printf("Enter command:\n");
-    scanf("%s", command); 
-    char *path;
-    path = malloc(20 * sizeof(char));
-    strcpy(path, "/bin/");
-    strcat(path, command);
-    execl(path,command ,NULL);
+    char *commands[3];
+    commands[0] = "ls";
+    commands[1] = "-l";
+    commands[2] = NULL;
+    // printf("Enter command:\n");
+    // scanf("%s", command); 
+    // char *path;
+    // path = malloc(20 * sizeof(char));
+    // strcpy(path, "/bin/");
+    // strcat(path, command);
+    
 
+    // execl(path,command ,NULL);
+    // free(path);
+
+    execv("/bin/ls",commands);
     return 0;
 }
