ca3fa57d8eeb43780f6d98bfa5b1a0cb09d5c91a
Anthony <mr.anthonygsoto@gmail.com>
2018-10-25 21:23:12 -0700

    Shortened variable names.

--- project-02/part3/airline.c
+++ project-02/part3/airline.c
@@ -4,26 +4,30 @@
 #include "passenger.h"
 
 int main(int argc, char *argv[]) {
-	int numberOfPassengers = *argv[1] - '0';
-//	int numberOfBaggageHandlers = *argv[2] - '0';
-//	int numberOfSecurityScreeeners = *argv[3] - '0';
-//	int numberOfFlightAttendents = *argv[4] - '0';
+	int numOfPass = *argv[1] - '0';
+//	int numOfBag = *argv[2] - '0';
+//	int numOfSec = *argv[3] - '0';
+//	int numOfAtten = *argv[4] - '0';
 	passenger *p = (passenger *) malloc(sizeof(passenger) * 10);
-	p = createPassengers(numberOfPassengers);
-	int passengerNumber = p[0].number;		
-	printf("Passenger #%d\n", passengerNumber);
+	p = createPassengers(numOfPass);
+	int passNum = p[0].number;		
+	p[0].number =699;
+	printf("Passenger #%d\n", passNum);
+	passNum = p[0].number;
+	printf("Passenger #%d\n", passNum);
 
-	int numberReturned[numberOfPassengers];
-	pthread_t passengerThread[numberOfPassengers];
 
-//  int numberReturned[numberOfBaggageHandlers];
-//  pthread_t baggageHandlerThread[numberOfBaggageHandlers];
+	int numPReturned[numOfPass];
+	pthread_t pThread[numOfPass];
+
+//  int numBReturned[numOfBag];
+//  pthread_t bThread[numOfBag];
   
-// int numberReturned[numberOfSecurityScreeners];
-//  pthread_t securityScreenerThread[numberOfSecurityScreeners];
+// int numSReturned[numOfSec];
+//  pthread_t sThread[numOfSec];
 
-//int numberReturned[numberOfFlightAttendents];
-//  pthread_t flightAttendentThread[numberOfFlightAttendents];
+//int numAReturned[numOfAtten];
+//  pthread_t aThread[numOfAtten];
 
 	return 0;
 }
