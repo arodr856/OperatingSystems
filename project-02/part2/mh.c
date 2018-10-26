6330101f21120f7c8800edfc36ce513581be4ea7
arodr856 <arodr856@gmail.com>
2018-10-26 12:29:43 -0700

    cycles are determined by command line arguement

--- project-02/part2/mh.c
+++ project-02/part2/mh.c
@@ -18,6 +18,7 @@ sem_t *father_sem;
 
 int main(int argc, char **argv){
 
+    int number_of_cycles = atoi(argv[1]);
     
     mother_mutex = sem_open("m_sem", O_CREAT, 0644, 1);
     father_sem = sem_open("f_sem", O_CREAT, 0644, 0);
@@ -25,8 +26,9 @@ int main(int argc, char **argv){
     pthread_t f_thread;
     pthread_t m_thread;
 
-    pthread_create(&m_thread, NULL, mother_thread, NULL);
-    pthread_create(&f_thread, NULL, father_thread, NULL);
+    pthread_create(&m_thread, NULL, mother_thread, &number_of_cycles);
+    pthread_create(&f_thread, NULL, father_thread, &number_of_cycles);
+    
     
     pthread_join(m_thread, NULL);
     pthread_join(f_thread, NULL);
@@ -38,14 +40,15 @@ int main(int argc, char **argv){
     return 0;
 }
 
-void *mother_thread(void *vargp){ 
+void *mother_thread(void *varg){ 
     
+    int cycles = *((int *) varg);
     
-    for(int day = 0; day < 100; day++){
+    for(int day = 0; day < cycles; day++){
         sem_wait(mother_mutex);
-        printf("this is day #%d of a day in the life of Mother Hubbard\n", (day + 1));
+        printf("This is day #%d of a day in the life of Mother Hubbard\n\n", (day + 1));
         for(int task = 0; task < 5; task++){
-            for(int child = 0; child < 3; child++){
+            for(int child = 0; child < 2; child++){
                 printf("Child #%d was %s\n\n", (child + 1), mother_tasks[task]);
                 usleep(100);
                 if(task == 4){
@@ -57,10 +60,13 @@ void *mother_thread(void *vargp){
     return NULL;  
 } 
 
-void *father_thread(void *vargp){ 
+void *father_thread(void *varg){ 
+
+    int cycles = *((int *) varg);
+
     // sem_wait(father_sem);
-    for(int day = 0; day < 100; day++){
-        for(int child = 0; child < 3; child++){
+    for(int day = 0; day < cycles; day++){
+        for(int child = 0; child < 2; child++){
             sem_wait(father_sem);
             for(int task = 0; task < 2; task++){
                 printf("Child #%d was %s\n\n", (child + 1), father_tasks[task]);
