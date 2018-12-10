c6fe1000c27d06832cef433edb7fc366547e7201
Eduardo Gutierrez Castelan <eduardogutierrez521@gmail.com>
2018-12-09 21:37:19 -0800

    Update client.c

--- project-03/part1/client.c
+++ project-03/part1/client.c
@@ -1,5 +1,4 @@
 
-// Client side C/C++ program to demonstrate Socket programming
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -24,7 +23,6 @@ int main(int argc, char const *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0){
         printf("\nInvalid address/ Address not supported \n");
         return -1;
