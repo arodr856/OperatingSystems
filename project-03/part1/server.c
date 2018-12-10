f5eecd9bbe6313e593ac279d39f45230b2f6ef5d
Eduardo Gutierrez Castelan <eduardogutierrez521@gmail.com>
2018-12-09 23:50:19 -0800

    Update server.c

--- project-03/part1/server.c
+++ project-03/part1/server.c
@@ -1,51 +1,71 @@
-
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
+ 
 #include <netinet/in.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <string.h> 
-#define PORT 8080 
-int main(int argc, char const *argv[]) 
+#include <sys/socket.h> 
+#include <unistd.h> 
+  
+#define PORT 8090 
+  
+// Driver code 
+int main() 
 { 
     int server_fd, new_socket, valread; 
     struct sockaddr_in address; 
-    int opt = 1; 
+    char str[100]; 
     int addrlen = sizeof(address); 
-    char buffer[1024] = {0}; 
-    char *hello = "good morning from server"; 
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){ 
-        perror("socket failed"); 
+    char buffer[1024] = { 0 }; 
+    char* hello = "HELLO WORLD"; 
+  
+    // Creating socket 
+    if ((server_fd = socket(AF_INET,  
+                          SOCK_STREAM, 0)) == 0) { 
+        perror("socket not connected"); 
         exit(EXIT_FAILURE); 
     } 
-       
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt))){ 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    } 
-
+  
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
-       
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0){ 
+    address.sin_port = htons(PORT); 
+  
+    // the port 8090 
+    if (bind(server_fd, (struct sockaddr*)&address,  
+                          sizeof(address)) < 0) { 
         perror("bind failed"); 
         exit(EXIT_FAILURE); 
     } 
-    if (listen(server_fd, 3) < 0){ 
+  
+    // the socket is on waiting mode  
+    if (listen(server_fd, 3) < 0) { 
         perror("listen"); 
         exit(EXIT_FAILURE); 
     } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0){ 
+    if ((new_socket = accept(server_fd, 
+                  (struct sockaddr*)&address, 
+                  (socklen_t*)&addrlen)) < 0) { 
         perror("accept"); 
         exit(EXIT_FAILURE); 
     } 
-    
-    valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello sent\n"); 
+  
+    // read string send by client.c 
+    valread = read(new_socket, str, 
+                   sizeof(str)); 
+    int i, j, temp; 
+    int l = strlen(str); 
+  
+    printf("\nThe strin that was sent by client.c:%s\n", str); 
+  
+    // loop to reverse the string and store it  
+    for (i = 0, j = l - 1; i < j; i++, j--) { 
+        temp = str[i]; 
+        str[i] = str[j]; 
+        str[j] = temp; 
+    } 
+  
+    //using system call 
+    send(new_socket, str, sizeof(str), 0); 
+    printf("\nReverse string sent\n"); 
+  
     return 0; 
-}
+} 
